2fd069feb0a7e93b5e00d05a25b17255c01df7bb
LePage73 <lepage@simbirsk.ru>
2024-09-13 19:34:49 +0400

    Сдаю

---
+++ game_store/task1/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+CHOICE_GENRE = [
+        ('FPS','Стрелялка'),
+        ('RPG','Ролевая игра'),
+        ('RTS','Стратегия в реальном времени'),
+        ('ARC','Аркада'),
+        ('SIM','Симулятор'),
+        ('ADV','Приключения'),
+    ]
+
+class Buyer(models.Model):
+    username = models.CharField(max_length=30)
+    balance = models.DecimalField(max_digits=12, decimal_places=2)
+    age = models.IntegerField(default=1)
+    pass
+
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    cost = models.DecimalField(max_digits=7, decimal_places=2)
+    size = models.DecimalField(max_digits=12, decimal_places=3)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='games')
+    genre = models.CharField(choices=CHOICE_GENRE, max_length=3)
+    pass
+
